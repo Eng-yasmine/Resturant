5f676a626347f9a1c6d4709be5919da3d7fe9249
eng-yasmine <yasmeen144@yahoo.com>
2025-05-01 23:38:54 +0300

    laravel telescope

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,13 @@ Route::controller(PageController::class)->prefix('User.pages')->as('pages.')->gr
 
 // });
 
-Route::get('/dashboard',[HomeController::class,'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard',[HomeController::class,'index'])->name('dashboard');
 });
 
 require __DIR__ . '/auth.php';
